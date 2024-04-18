dd6bd5d0005f500d09eb2a774134c26766de7c52
ERIK HERMANN RADTKE <erik_radtke@estudante.sesisenai.org.br>
2024-04-18 13:55:59 -0300

    Instalado Bootstrap

--- dependence.py
+++ dependence.py
@@ -1,13 +1,13 @@
 import os 
 
-comando = "pip install django && pip install mysqlclient && pip install Pillow && npm install -g sass"
+comando = "pip install django && pip install mysqlclient && pip install Pillow && npm install -g sass && npm i bootstrap@5.3.3"
 
 def dependenceInstall(comando):
     os.system(comando)
 
 dependenceInstall(comando) #Executando a instalação
 
-comando_dbs = "mysql -u root -p < database.sql" #Comando que insere a database.sql diretamente no mysql
-def insert_DBS(comando_dbs):
-    os.system(comando_dbs) #Permite a execução no terminal
-insert_DBS(comando_dbs) #Chama a execução
+# comando_dbs = "mysql -u root -p < database.sql" #Comando que insere a database.sql diretamente no mysql
+# def insert_DBS(comando_dbs):
+#     os.system(comando_dbs) #Permite a execução no terminal
+# insert_DBS(comando_dbs) #Chama a execução
