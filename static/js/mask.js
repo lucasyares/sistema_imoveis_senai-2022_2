96a929c72b0b809d5ca02df1912e895672b1154a
lucasyaressilva <lucas.silva@webfloat.com.br>
2024-05-18 19:40:50 -0300

    Comentarios desativos em arquivos js

--- static/js/mask.js
+++ static/js/mask.js
@@ -1,17 +1,4 @@
 "use strict";
-// StringHelper possuí todas as nossas masks
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//DEVE ALTERAR AS MASKS SOMENTE AQUI!
-//COMANDO PARA ALTERAR -> ts -w
-//
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//COLOCAR NO HTML(como chamar)
-// oninput="this.value = mask(this.value, 'type?????')"
-//
-//exemplo:
-// <input type="text" oninput="this.value = mask(this.value, 'telefone')">
-////////////////////////
 function mask(a, type) {
     if (type === "telefone") {
         const phone = a.replace(/\D/g, '');
@@ -25,9 +12,9 @@ function mask(a, type) {
     }
     else if (type === "cpf") {
         return a
-            .replace(/\D/g, '') // Remove todos os caracteres que não são dígitos
-            .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona um ponto após os primeiros três dígitos
-            .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona outro ponto após os próximos três dígitos
+            .replace(/\D/g, '')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
             .replace(/(\d{3})(\d{1,2})/, '$1-$2');
     }
     else if (type === "cep") {
@@ -36,12 +23,9 @@ function mask(a, type) {
             a = a.replace(/^(\d{5})/, '$1-');
         }
     }
-    else if (type === "dinheiro") {
-        // Remover todos os caracteres não numéricos
+    else if (type === "money") {
         a = a.replace(/\D/g, '');
-        // Converter para número
-        let numero = parseFloat(a) / 100; // Assumindo que o valor está em centavos
-        // Formatando para o formato de moeda brasileira (Real)
+        let numero = parseFloat(a) / 100;
         a = numero.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     }
     return a;
