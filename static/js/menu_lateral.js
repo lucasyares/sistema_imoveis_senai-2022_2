2983adf71e40443f9fadddb2dc5fbdc39b6071c3
hrerik <radtke.erik@gmail.com>
2024-06-11 22:30:17 -0300

    Estilizando CRM #1

--- static/js/menu_lateral.js
+++ static/js/menu_lateral.js
@@ -2,22 +2,12 @@ const menuLateral = document.getElementById("menu-lateral");
 const fundo = document.getElementById("menu-fundo");
 
 function exibirMenuLateral() {
-    // menuLateral.style.display = "block";
-    menuLateral.style.width = "30vw";
+    menuLateral.style.right = "0"
     fundo.style.display = "block";
-
-    // var time = 2000;
-    // setTimeout(function () {
-    //     for (let index = 0; index < 100; index++) {
-    //         menuLateral.style.width = index + "%";
-    //         console.log("cu");
-    //     }
-    // }, time);
 }
 
 
 function esconderMenuLateral() {
-    // menuLateral.style.display = "none";
-    menuLateral.style.width = "0";
+    menuLateral.style.right = "-30vw"
     fundo.style.display = "none";
 }
